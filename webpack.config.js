17ce4167ed749bd135ee71b1c6921ff16b05f6ab
Le Minh Tri <ansidev@gmail.com>
2018-12-15 18:26:21 +0700

    Upgrade dependencies and migrate webpack + babel config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
-var path = require('path')
-var webpack = require('webpack')
-
+const path = require('path')
+const webpack = require('webpack')
+const devMode = process.env.NODE_ENV !== 'production';
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -21,6 +25,13 @@ module.exports = {
             'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
           }
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          devMode ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
       },
       {
         test: /\.js$/,
@@ -44,6 +55,21 @@ module.exports = {
       'vue$': 'vue/dist/vue.esm.js'
     }
   },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        sourceMap: true,
+        parallel: 4,
+        uglifyOptions: {
+          warnings: false,
+          compress: {
+            warnings: false
+          },
+        },
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
   devServer: {
     historyApiFallback: true,
     noInfo: true
@@ -53,6 +79,11 @@ module.exports = {
   },
   devtool: '#source-map',
   plugins: [
+    new VueLoaderPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'vueup.css',
+      disable: process.env.NODE_ENV === 'development'
+    }),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"'
