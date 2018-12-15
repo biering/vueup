69bad1613b68a7f3983e2490cdd7e6bc1da80aa0
Le Minh Tri <ansidev@gmail.com>
2018-12-15 18:25:30 +0700

    Change output file name

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'index.js',
+    filename: 'vueup.js',
     library:'vue-notification',
     libraryTarget: 'umd'
   },
